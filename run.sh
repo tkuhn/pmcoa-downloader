03bef89d182508ed26c1719920949a70c1117469
Tobias Kuhn <kuhntobias@gmail.com>
2013-06-14 09:16:15 +0200

    Download xml and csv too

--- run.sh
+++ run.sh
@@ -12,20 +12,51 @@ rm -f PROCESSING
 echo "DOWNLOAD FILE LIST"
 #     ------------------
 
-mkdir -p filelist
-if [ ! -e filelist/$TODAY.txt ] ; then
-  wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/file_list.txt -O filelist/$TODAY.txt
+mkdir -p lists
+
+if [ ! -e lists/file_list-$TODAY.txt ] ; then
+  echo "file_list.txt" > PROCESSING
+  wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/file_list.txt -O lists/file_list-$TODAY.txt
 fi
-if [ ! -e filelist/$TODAY.txt ] ; then
-  echo "ERROR: Could not find or download file list"
+if [ ! -e lists/file_list-$TODAY.txt ] ; then
+  echo "ERROR: Could not find or download file_list.txt"
   exit
+fi
+
+if [ ! -e lists/PMC-ids-$TODAY.csv ] ; then
+  echo "PMC-ids.csv.gz" > PROCESSING
+  wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/PMC-ids.csv.gz -O lists/PMC-ids.csv.gz
+  gunzip lists/PMC-ids.csv.gz
+  mv lists/PMC-ids.csv lists/PMC-ids-$TODAY.csv
+fi
+if [ ! -e lists/PMC-ids-$TODAY.csv ] ; then
+  echo "ERROR: Could not find or download PMC-ids.csv"
+  exit
+fi
+
+
+echo "DOWNLOAD XML"
+#     ------------
+
+mkdir -p xml
+
+if [ ! -e xml/$TODAY ] ; then
+  mkdir -p xml/$TODAY
+  echo "articles.*.tar.gz" > PROCESSING
+  wget --directory-prefix="xml/$TODAY" ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/articles.*.tar.gz
+  for f in xml/$TODAY/*.tar.gz ; do
+    n=$(basename "$f")
+    n="${n%.tar.gz}"
+    mkdir -p xml/$TODAY/$n
+    tar xfz "$f" -C "xml/$TODAY/$n"
+  done
 fi
 
 
 echo "PREPROCESS FILE LIST"
 #     --------------------
 
-cat filelist/$TODAY.txt | sed -e '1,1 d' | awk '{ print $1 }' | sort > file_list.tmp
+cat lists/file_list-$TODAY.txt | sed -e '1,1 d' | awk '{ print $1 }' | sort > file_list.tmp
 NFILES=`cat file_list.tmp | wc -l`
 
 
